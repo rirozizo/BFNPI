35ac09f8ed58f85b616ab1c031aaaa1984db4268
rirozizo <rawadzahreddine32@gmail.com>
2020-10-24 18:49:34 +0300

    python3 instead of python

--- installer.sh
+++ installer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Installing google play scraper"
-pip install google-play-scraper
+pip3 install google-play-scraper
 echo "Installing tweepy"
-pip install tweepy
+pip3 install tweepy
 echo "First-Time setup"
-python setup.py
+python3 setup.py
 echo "Done! Now run scrape.py whenever you want to check for any Google app update"
 echo "Don\'t forget to put your API KEY, SECRET, and ACCESS TOKEN and SECRET inside scrape.py!!"
